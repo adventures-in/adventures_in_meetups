87d1dc2b2c33818916a3c1e289f12c05fd68fb74
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-06 23:35:40 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# adventures_in_meetups
+
+[Notes](https://docs.google.com/document/d/1M3jSaD6v0GxtUofH-YUhswVG6Kvv-C-4mBad36_cijM/edit?usp=sharing)
---
+++ android/app/src/main/kotlin/co/enspyr/adventures_in_meetups/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.enspyr.adventures_in_meetups
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
