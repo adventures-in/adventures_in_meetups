dd88e4653127f0db91bca1a774a698603fe0f27a
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-07 19:23:10 +1000

    Update function to send request body in response

--- microservices/authorizer/lib/functions.dart
+++ microservices/authorizer/lib/functions.dart
@@ -2,4 +2,5 @@ import 'package:functions_framework/functions_framework.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
-Response function(Request request) => Response.ok('Hello, World!');
+Future<Response> function(Request request) async =>
+    Response.ok(await request.readAsString());
